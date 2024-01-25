cc0221e51246548cc500ffb22ac00115b8bd2326
Nitsan Caduri <nitsan447@gmail.com>
2024-01-25 22:11:55 +0200

    3d scene

---
+++ Assets/GGJ 2024/Player/GoofyController.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoofyController : MonoBehaviour
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public bool OnGround => _groundContactCount > 0;
+
+        [SerializeField, Range(0f, 100f)] private float _maxSpeed = 10f;
+        [SerializeField, Range(0f, 100f)] private float _acceleration = 10f;
+        [SerializeField, Range(0f, 90f)] private float _maxGroundAngle = 25f;
+        [SerializeField, Range(0f, 100f)] private float _maxSpeedToSnapToGround = 100f;
+        [SerializeField, Min(0f)] private float _probeDistance = 1f;
+        [SerializeField] private LayerMask _probeMask = -1;
+
+        private Vector3 _velocity;
+        private Vector3 _desiredVelocity;
+        private Rigidbody _rigidBody;
+        private float _minGroundDotProduct;
+        private Vector3 _contactPointNormal;
+        private int _physicsStepsSinceLastGrounded;
+        private int _groundContactCount;
+
+        private void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+            OnValidate();
+        }
+
+        private void OnValidate()
+        {
+            _minGroundDotProduct = Mathf.Cos(_maxGroundAngle * Mathf.Deg2Rad);
+        }
+
+        private void Update()
+        {
+            Vector2 directionalInput = Vector2.ClampMagnitude(InputManager.Instance.DirectionalInput, 1);
+
+
+            _desiredVelocity =
+                new Vector3(directionalInput.x, 0f, directionalInput.y) * _maxSpeed;
+        }
+
+
+        private void FixedUpdate()
+        {
+            UpdateState();
+            AdjustVelocity();
+
+            _rigidBody.velocity = _velocity;
+            ClearState();
+        }
+
+        private void UpdateState()
+        {
+            _physicsStepsSinceLastGrounded++;
+            _velocity = _rigidBody.velocity;
+            if (OnGround || SnapToGround())
+            {
+                _physicsStepsSinceLastGrounded = 0;
+                if (_groundContactCount > 1)
+                {
+                    _contactPointNormal.Normalize();
+                }
+            }
+            else
+            {
+                _contactPointNormal = Vector3.up;
+            }
+        }
+
+        private void AdjustVelocity()
+        {
+            Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+            Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+            float currentXVelocity = Vector3.Dot(_velocity, xAxis);
+            float currentZVelocity = Vector3.Dot(_velocity, zAxis);
+
+            float maxSpeedChange = _acceleration * Time.deltaTime;
+
+            float newXVelocity = Mathf.MoveTowards(currentXVelocity, _desiredVelocity.x, maxSpeedChange);
+            float newZVelocity = Mathf.MoveTowards(currentZVelocity, _desiredVelocity.z, maxSpeedChange);
+
+            _velocity += xAxis * (newXVelocity - currentXVelocity) + zAxis * (newZVelocity - currentZVelocity);
+        }
+
+        private Vector3 ProjectOnContactPlane(Vector3 vector)
+        {
+            return vector - _contactPointNormal * Vector3.Dot(vector, _contactPointNormal);
+        }
+
+        private bool SnapToGround()
+        {
+            if (_physicsStepsSinceLastGrounded > 1)
+            {
+                return false;
+            }
+
+            float speed = _velocity.magnitude;
+            if (speed > _maxSpeedToSnapToGround)
+            {
+                return false;
+            }
+
+
+            if (!Physics.Raycast(_rigidBody.position, Vector3.down, out RaycastHit hit, _probeDistance, _probeMask))
+            {
+                return false;
+            }
+
+            if (hit.normal.y < _minGroundDotProduct)
+            {
+                return false;
+            }
+
+            _groundContactCount = 1;
+            _contactPointNormal = hit.normal;
+
+            float dot = Vector3.Dot(_velocity, hit.normal);
+            if (dot > 0)
+            {
+                _velocity = (_velocity - hit.normal * dot).normalized * speed;
+            }
+
+            return true;
+        }
+
+        private void ClearState()
+        {
+            _groundContactCount = 0;
+            _contactPointNormal = Vector3.zero;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            EvaluateCollision(other);
+        }
+
+        private void OnCollisionStay(Collision other)
+        {
+            //Called after fixed update
+            EvaluateCollision(other);
+        }
+
+        private void EvaluateCollision(Collision other)
+        {
+            for (int i = 0; i < other.contactCount; i++)
+            {
+                Vector3 normal = other.GetContact(i).normal;
+                if (normal.y >= _minGroundDotProduct)
+                {
+                    _groundContactCount++;
+                    _contactPointNormal += normal;
+                }
+            }
+        }
+    }
+}
