08f7297b0c7fd1b344378ff1c094d964c941eae6
Nitsan Caduri <nitsan447@gmail.com>
2024-01-28 14:58:07 +0200

    Fixed keys remaining on restart

--- Assets/Template/Singletons/SceneReferencer/SceneReferencer.cs
+++ Assets/Template/Singletons/SceneReferencer/SceneReferencer.cs
@@ -7,13 +7,5 @@ public class SceneReferencer : ASingleton<SceneReferencer>
 {
     public Player Player;
     public Danger Danger;
-
-    protected override void DoOnAwake()
-    {
-        base.DoOnAwake();
-        if (Player == null)
-        {
-            Player = FindObjectOfType<Player>();
-        }
-    }
+    public GameObject KeysParent;
 }
